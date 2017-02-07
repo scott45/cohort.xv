070168b06a529d8dd1c03aaecd07590ca455d737
Scott B5 <busingescott@gmail.com>
2017-02-07 12:40:18 +0300

    final commit oop

--- day-1/oop.py
+++ day-1/oop.py
@@ -4,14 +4,13 @@ __author__ = 'Scott Businge'
 # OOP real world example was demonstrating OOP through Andela boot camp
 
 class Bootcamp(object):  # super class (main).
-    def __init__(self, cohort, group, bcf):  # instantiation
+    def __init__(self, cohort, group,):  # instantiation
         self.cohort = cohort
         self.group = group
-        self.bcf = bcf
 
 
 class Bootcamper(Bootcamp):  # inheritance                            # second class that inherits from super.
-    def __init__(self, cohort, group, first_name, last_name, gender, pay, ):
+    def __init__(self, cohort, group, first_name, last_name, gender, ):
         super().__init__(cohort, group)
         self.first_name = first_name  # other class declarations not main.
         self.last_name = last_name
@@ -22,12 +21,12 @@ class Bootcamper(Bootcamp):  # inheritance                            # second c
         raise NotImplementedError("Subclass must implement abstract method")
 
 
-entry1 = Bootcamper(15, 8, 'scott', 'bb', 'M', )           # instances
+entry1 = Bootcamper(15, 8, 'scott', 'bb', 'M', )  # instances
 entry2 = Bootcamper(15, 8, 'kayuyu', 'kk', 'F', )
 entry3 = Bootcamper(15, 8, 'vivian', 'vv', 'F', )
 entry4 = Bootcamper(15, 8, 'brenda', 'ba', 'F', )
 
-print(entry1.first_name)                                                             # print returns
+print(entry1.first_name)  # print returns
 print(entry2.group)
 print(entry3.cohort)
 print(entry4.gender)
