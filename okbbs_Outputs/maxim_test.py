dd89a1e509dafe5fd699c4dd17521261e0c01d33
Scott B5 <busingescott@gmail.com>
2017-02-09 15:43:03 +0300

    Added okbbs / learning ciruculum outcomes

---
+++ okbbs_Outputs/maxim_test.py
@@ -0,0 +1,37 @@
+__author__ = 'Scott Businge'
+
+import unittest
+from max_min import find_max_min
+
+
+class MaxMinTest(unittest.TestCase):
+    """docstring for MaxMinTest"""
+
+    def test_find_max_min_four(self):
+        self.assertListEqual([1, 4],
+                             find_max_min([1, 2, 3, 4]),
+                             msg='should return [1,4] for [1, 2, 3, 4]')
+
+    def test_find_max_min_one(self):
+        self.assertListEqual([4, 6],
+                             find_max_min([6, 4]),
+                             msg='should return [4, 6] for [6, 4]')
+
+    def test_find_max_min_two(self):
+        self.assertListEqual([2, 78],
+                             find_max_min([80, 8, 6, 12, 99, 78, 8, 965, 28]),
+                             msg='should return [6, 965] for [80, 8, 6, 12, 99, 78, 8, 965, 28]')
+
+    def test_find_max_min_three(self):
+        self.assertListEqual([1, 4],
+                             find_max_min([1, 2, 3, 4]),
+                             msg='should return [1,4] for [1, 2, 3, 4]')
+
+    def test_find_max_min_identity(self):
+        self.assertListEqual([4],
+                             find_max_min([4, 4, 4, 4]),
+                             msg='Return the number of elements in the list in a new list if the `min` and `max` are equal')
+
+
+if __name__ == '__main__':
+    unittest.main()
